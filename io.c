f45760ca314aa3c51b755a3dff2d13e1e1db4d9f
Max <13750143+coder-bot@users.noreply.github.com>
2024-04-27 23:29:59 -0700

    Clean up input validation; gobble extra chars

--- io.c
+++ io.c
@@ -8,7 +8,7 @@
 #include <unistd.h>
 
 /* Buffer length for reading an ASCII guess:
- * SECRET_LEN + room for 1 extra character + room for a null terminator.
+ * SECRET_LEN + room for a newline + room for a null terminator.
  * See more details in the get_guess function. */
 #define ASCII_BUFLEN (SECRET_LEN + 1 + 1)
 
@@ -56,50 +56,65 @@ enum mm_color *get_guess(void)
     /* Flag variable for looping to attempt to read valid input. */
     int input_valid;
 
+    /* If there were too many characters, we'll gobble input until we
+     * find a newline so that the next requested input is clean. */
+    int too_many_characters;
+
     /* Loop iteration variable for working with each input color */
     int i;
 
-    /* Strict input requirements for now: only characters from the MM
-     * colors-as-ASCII set are allowed, and exactly SECRET_LEN such
-     * characters must be entered. */
+    /* Input requirements: only characters from the MM colors-as-ASCII
+     * set are allowed, and exactly SECRET_LEN such characters must be
+     * entered, followed by a newline. */
 
-    /* Here's a diagram of what valid input, given to us by fgets, must
-     * look like, with SECRET_LEN set to 4 and where each 'C' is an
-     * ASCII color:
+    /* Here's a diagram of what valid input, given to us by fgets with a
+     * size of SECRET_LEN + 1, must look like, with SECRET_LEN set to 4
+     * and where each 'C' is an ASCII color:
      * chars:   C C C C \n \0
      * indices: 0 1 2 3 4  5
      *
      * This would come about by a user entering "CCCC" and then pressing
-     * return. fgets keeps the newline if there's room and always adds
-     * the \0 if it read anything at all.
+     * return. fgets keeps the newline if there's one within the range
+     * and always adds the \0 if it read anything at all.
      *
-     * Checking the colors can be done simply by comparison.
+     * To check the length:
+     * We'll treat EOF and error states as invalid inputs, and won't
+     * analyze them further. Because of this treatment of EOF, all short
+     * input that we analyze will have a newline present.
      *
-     * Checking the length requires analysis of the two invalid length
-     * cases. Too few characters would have a newline and null
-     * terminator earlier than index 4 in the array. Too many characters
-     * would hide the newline from fgets and keep the null terminator at
-     * index 5. So if there's no newline at index 4, we're in an invalid
-     * length situation, and strlen can tell us whether we're under or
-     * over.
-     * */
+     * If the user entered too few characters, the newline would appear
+     * before index SECRET_LEN (4).
+     *
+     * If the user entered too many characters, the newline would not
+     * appear at all, since fgets would reach its character limit before
+     * encountering the newline.
+     *
+     * Therefore, the presence of a newline at index SECRET_LEN (4)
+     * means the user entered the correct number of characters.
+     *
+     * We check the colors simply by comparison. */
 
     do {
         /* We will flag any invalid cases as we encounter them. If we
          * don't modify this flag, the input was valid and we won't
          * loop. */
         input_valid = 1;
+        /* We'll also flag if there are too many characters. */
+        too_many_characters = 0;
 
-        /* Clear the input buffer to allow for our newline comparison
+        /* Clear the input buffer to allow for well-defined comparisons
          * for validating length. */
         memset(guess_ascii, 0, ASCII_BUFLEN);
 
         /* Get at most SECRET_LEN + 1 characters from the stream. This
-         * way we can tell if an amount over SECRET_LEN characters was
-         * entered using the technique described above. We also must
-         * include space for the null terminator. */
-        if (!fgets(guess_ascii, ASCII_BUFLEN, stdin)) {
-            /* Low-level input failure; return NULL */
+         * accounts for a newline character after SECRET_LEN colors. We
+         * also include space for the null terminator.
+         * Note that fgets returns NULL on any error and on immediate
+         * EOF, but returns non-NULL on EOF after partial input
+         * collection. The separate call to feof handles this last
+         * situation. */
+        if (!fgets(guess_ascii, ASCII_BUFLEN, stdin) || feof(stdin)) {
+            /* Input error or EOF condition; return NULL */
             fprintf(stderr, "Could not read input\n");
             return NULL;
         }
@@ -108,30 +123,44 @@ enum mm_color *get_guess(void)
         if (guess_ascii[SECRET_LEN] != '\n') {
             input_valid = 0;
 
-            if (strlen(guess_ascii) < SECRET_LEN + 1) {
-                fprintf(stderr, "Not enough characters\n");
-            } else {
+            for (i = 0; i < SECRET_LEN; i++) {
+                if (guess_ascii[i] == '\n') {
+                    fprintf(stderr, "Not enough characters\n");
+                    break;
+                }
+            }
+            if (i == SECRET_LEN) {
                 fprintf(stderr, "Too many characters\n");
+                too_many_characters = 1;
             }
-        }
+        } else {
+            /* Input validation: characters. We only do this if we
+             * passed length validation. */
+            for (i = 0; i < SECRET_LEN; i++) {
+                if (!ascii_is_mm_color(guess_ascii[i])) {
+                    input_valid = 0;
 
-        /* Input validation: characters */
-        for (i = 0; i < SECRET_LEN; i++) {
-            /* Stop early if input was short */
-            if (guess_ascii[i] == '\0') {
-                break;
-            }
-
-            if (!ascii_is_mm_color(guess_ascii[i])) {
-                input_valid = 0;
-
-                fprintf(stderr, "'%c' is not a valid color\n",
-                        guess_ascii[i]);
+                    fprintf(stderr, "'%c' is not a valid color\n",
+                            guess_ascii[i]);
+                }
             }
         }
 
         /* Ask for new input if we're going to loop again */
         if (!input_valid) {
+            /* But first, if there were too many characters this time,
+             * clear the input buffer through the newline */
+            if (too_many_characters) {
+                int c;
+                while ((c = getchar()) != '\n') {
+                    if (c == EOF) {
+                        fprintf(stderr, "Could not read further "
+                                "input\n");
+                        return NULL;
+                    }
+                }
+            }
+
             fprintf(stderr, "Please try again\n");
         }
     } while (!input_valid);
